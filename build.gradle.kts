1b1e22553a301f75f4af5d19adff618e95b43247
Dreamyboyyt <pkpakisanp804@gmail.com>
2025-06-14 22:36:28 +0500

    Overwrite everything with latest local version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,20 @@
-import com.android.build.api.dsl.ManagedVirtualDevice
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
